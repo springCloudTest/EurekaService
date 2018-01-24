d9ad18753332a97a2b4bc87e742bef5f9ce24308
zhengxiaobin <hubert@12138gmail.com>
2018-01-25 00:50:24 +0800

    localhost:1111

--- src/main/java/com/hubert/EurekaService/EurekaServiceApplication.java
+++ src/main/java/com/hubert/EurekaService/EurekaServiceApplication.java
@@ -2,7 +2,9 @@ package com.hubert.EurekaService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
